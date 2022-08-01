6e7ad109189c41900f07d36b5aa5fe24d90399d9
delaying <pgy5638@naver.com>
2022-08-01 02:38:31 +0100

    #2 starbucks clone coding - visual, notice-promotion

--- starbucks-hompage-clone/js/main.js
+++ starbucks-hompage-clone/js/main.js
@@ -49,3 +49,51 @@ window.addEventListener(
     }
   }, 300)
 );
+
+// 메인페이지 각각의 이미지 애니메이션 주기
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  // 애니메이션 처리 라이브러리 gsap.to(요소,지속시간(초단위),옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7, //0.7, 1.4, 2.1, 2.7 index순서마다 이 시간의 간격으로 진행됨. index 0번은 1*0.7
+    opacity: 1,
+  });
+});
+
+//swiper 제어, new Swiper(선택자,옵션)
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000, //5초
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+// toggle promotion
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add("hide");
+  } else {
+    promotionEl.classList.remove("hide");
+  }
+});
