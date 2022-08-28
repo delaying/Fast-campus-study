4931de67e7194e50262092d2f9de16658d1bd0f9
delaying <pgy5638@naver.com>
2022-08-28 14:59:56 +0900

    #8 JSON 형식과 메소드  연습

--- main.js
+++ main.js
@@ -1,57 +1,29 @@
-import _ from "lodash"; //lodash 는 _로우대쉬를 사용하라는것~!
-//구조 분해 할당
-//비구조화 할당
+//JSON
+
+import myData from "./myData.json";
+
+console.log(myData);
+
 const user = {
   name: "jiyeon",
   age: 23,
-  email: "pgy5638@naver.com",
+  emails: ["pgy5638@naver.com", "neo@naver.com"],
 };
-const { name, age, email, address } = user;
-//E.g, user.address
-
-console.log(`사용자의 이름은 ${name}입니다.`);
-console.log(`${name}의 나이는 ${age}`);
-console.log(address);
-
-const fruits = ["Apple", "Banana", "Cherry", "orange"];
-const [a, b, c, d, e] = fruits;
-console.log(a, b, c, d);
-
-//전개 연산자
-console.log(fruits);
-console.log(...fruits);
-
-function toObject(a, b, ...c) {
-  return {
-    a: a,
-    b: b,
-    c: c,
-  };
-}
-
-console.log(toObject(...fruits));
-
-//데이터 불변성
-let k = { k1: 1 };
-let j = { k1: 4 };
-console.log(k, j, k === j);
-
-k.k1 = 7;
-j = k; //k메모리주소를 j가 바라보게되므로 true가 나옴
-console.log(k, j, k === j);
-
-k.k1 = 2; //j가 이미 k메모리를 바라보고있으므로 j의 k1값도 수정됨
-console.log(k, j, k === j);
-
-let i = j;
-console.log(k, j, i, k === i);
-k.k1 = 9;
-console.log(k, j, i, k === i);
-
-//얕은복사 ,깊은복사
-const copyUser = _.cloneDeep(user);
-console.log(copyUser === user); //false
-
-user.age = 22;
 console.log("user", user);
-console.log("copyUser", copyUser);
+
+const str = JSON.stringify(user);
+console.log("str", str);
+console.log(typeof str);
+
+const obj = JSON.parse(str);
+console.log("obj", obj);
+
+//Local Stroage
+// localStorage.setItem("user", JSON.stringify(user));
+// console.log(JSON.parse(localStorage.getItem("user")));
+
+const stri = localStorage.getItem("user");
+const obje = JSON.parse(stri);
+obje.age = 20;
+console.log(obje);
+localStorage.setItem("user", JSON.stringify(obje));
